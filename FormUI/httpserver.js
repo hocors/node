2c3f02d324c849b485d12d9d0125e97b40e3a966
hokers_home_bjb <706606599@qq.com>
2017-03-14 23:38:41 +0800

    express listen sendFile

--- FormUI/httpserver.js
+++ FormUI/httpserver.js
@@ -1,14 +1,35 @@
-﻿var hostname = '192.168.10.30';
-var hostport = '314';
-
-var express = require('express');
+﻿var express = require('express');
 var app = express();
-app.listen(hostport, function () {
-    console.log(`web service start     http://${hostname}:${hostport}/`);
+var fs = require('fs');
+var ejs = require('ejs');
+
+app.set('host', '127.0.0.1');//定义全局变量
+app.set('port', 600);//定义全局变量
+
+ap.engine('.html', ejs, __express);
+app.set('view engine', 'html');
+
+app.get('/form', function (req, res) {
+    res.status(200).sendFile(__dirname + '/Client/Views/form.html');// success
+    //res.status(200).render(__dirname + '/Client/Views/form.html', function (err, html) {
+    //    res.send(html);
+    //});
 });
 
+app.use(function (req, res) {
+    res.type('text/plain');
+    res.status(404);
+    res.send('页面被程序猿吃了！');
+});
+
+app.listen(app.get('port'), function () {
+    console.log(`web service start     http://${app.get('host')}:${app.get('port')}/`);
+});
+
+
+
 //var http = require('http');
-//var hostname = '192.168.10.30';
+//var hostname = '192.168.10.30
 //var hostport = '314';
 //var fs = require('fs');
 //var server = http.createServer((req, res) => {
