f3247ee469095704a5955ea719e970ddd4065591
hokers_home_bjb <706606599@qq.com>
2017-03-19 00:17:04 +0800

    statis resources load

--- FormUI/httpserver.js
+++ FormUI/httpserver.js
@@ -1,43 +1,96 @@
-﻿var app = require('express')();
+﻿//引入创建express模块服务
+var express = require('express');
+var app = express();
+
+//引入文件读取模块
 var fs = require('fs');
+
+//引入视图handlebars模板引擎
 var handlebars = require('express-handlebars').create({
     extname: 'html',
     defaultLayout: 'main',
-    layoutsDir: __dirname + '/Client/Views'
+    layoutsDir: __dirname + '/views',
+    helpers: {
+        section: function (name, options) {
+            if (!this._sections) {
+                this._sections = {};
+            }
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
 });
 
+//设置模板引擎
 app.engine('html', handlebars.engine);
 app.set('view engine', 'html');
 
-app.set('host', '127.0.0.1');//定义全局变量
-app.set('port', 600);//定义全局变量
+//定义Internet Information Services服务地址
+app.set('host', '127.0.0.1');
+app.set('port', 600);
 
+//定义资源地址目录
+app.set('resources', __dirname + '/public');
+
+//定义res上下文附加参数
+var resContext = {
+    jqueryBasePath: 'http://lib.sinaapp.com/js/jquery/1.9.1',
+    resourcesPath: ''
+}
+
+//创建资源路由
+app.use('/', express.static(app.get('resources')));
+
+//表单路由
 app.get('/form', function (req, res) {
-    res.status(200).sendFile(__dirname + '/Client/Views/form.html');//success
+    resContext['title'] = '表单组件开发&测试';
+    page.render(req, res, '/Views/form', resContext);
 });
 
+//测试使用handlebars模板引擎
 app.get('/index', function (req, res) {
-    res.status(200).render(__dirname + '/Client/Views/index', { info: '测试模板文件' }, function (err, html) {
-        if (err) {
-            res.type('text/plain').status(500).send('页面被程序猿吃了！');
-        }
-        else {
-            res.send(html);
-        }
-    });
+    resContext['title'] = '我的nodejs学习计划';
+    resContext['info'] = 'hello world ';
+    page.render(req, res, '/Views/index', resContext);
 });
 
+app.get('/', function (req, res) {
+    resContext['title'] = '我的nodejs学习计划';
+    resContext['info'] = 'hello world ';
+    page.render(req, res, '/Views/index', resContext);
+});
+
+//page 404错误
 app.use(function (req, res) {
     res.type('text/plain');
-    res.status(200);
+    res.status(404);
     res.send('页面被程序猿吃了！');
 });
 
+//page 500错误 Internet Information Services
+app.use(function (err, req, res, next) {
+    res.type('text/plain');
+    res.status(500);
+    res.send('访问异常，正千里传音呼叫程序猿上线排查！/r/n' + err);
+});
+
+//创建 监听 
 app.listen(app.get('port'), function () {
     console.log(`web service start     http://${app.get('host')}:${app.get('port')}/`);
 });
 
-
+var page = {
+    render: function (req, res, pageurl, resData) {
+        res.render(__dirname + pageurl, resContext, function (err, html) {
+            if (err) {
+                res.type('text/plain').status(500).send('访问异常，正千里传音呼叫程序猿上线排查！/r/n' + err);
+            }
+            else {
+                res.status(200).send(html);
+            }
+        });
+    }
+}
 
 //var http = require('http');
 //var hostname = '192.168.10.30
